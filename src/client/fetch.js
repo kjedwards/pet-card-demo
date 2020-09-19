8be4b7d56d41da06e753d32cef7f258fa05487f1
Kaylee Edwards <k.edwards1993@gmail.com>
2020-09-19 09:51:56 -0500

    Backup plan for fetching data works

--- src/client/fetch.js
+++ src/client/fetch.js
@@ -1,6 +1,41 @@
 const fetchIds = () => {
-    fetch('https://api.adoptapet.com/search/pet_search?output=json&key=e41b6bf1618d053c31d524d479c14b4y&geo_range=1&city_or_zip=Richmond,%20IN&species=dog')
-    .then( res => {
-        console.log(res)
-    })
+    fetch('data/petIds.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
+        })
+        .catch(function () {
+            cosnsole.log('error');
+        })
 }
+
+const fetchPet = pet => {
+    fetch(`data/${pet.pet_id}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
+        })
+        .catch(function () {
+            cosnsole.log('error');
+        })
+}
+
+// Unfortunately I ran into CORS issues with the API. I had only checked in Insomnia before the question window
+// was over. So, I copied the insomnia response into JSON files to simulate "calls"
+//
+// const fetchIds = () => {
+//     fetch('https://api.adoptapet.com/search/pet_search?output=json&key=e41b6bf1618d053c31d524d479c14b4y&geo_range=1&city_or_zip=Richmond,%20IN&species=dog')
+//     .then( res => {
+//         console.log(res)
+//     })
+//}
